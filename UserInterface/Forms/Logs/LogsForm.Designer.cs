16fb9ef9e7b4ab2e22423d495d3a23d64fd5b989
Tomasz Banach <tom.banach98@gmail.com>
2021-11-02 20:07:27 +0000

    LogsForm View Imported from Changeset 137

---
+++ UserInterface/Forms/Logs/LogsForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SmayDbEditor.UserInterface.Forms.Logs
+{
+    partial class LogsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpLogs = new System.Windows.Forms.TableLayoutPanel();
+            this.pLogs = new System.Windows.Forms.Panel();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tlpLogs.SuspendLayout();
+            this.pLogs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpLogs
+            // 
+            this.tlpLogs.ColumnCount = 1;
+            this.tlpLogs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpLogs.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpLogs.Controls.Add(this.pLogs, 0, 0);
+            this.tlpLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpLogs.Location = new System.Drawing.Point(0, 0);
+            this.tlpLogs.Name = "tlpLogs";
+            this.tlpLogs.RowCount = 2;
+            this.tlpLogs.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tlpLogs.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpLogs.Size = new System.Drawing.Size(661, 455);
+            this.tlpLogs.TabIndex = 0;
+            // 
+            // pLogs
+            // 
+            this.pLogs.Controls.Add(this.textBox1);
+            this.pLogs.Controls.Add(this.label1);
+            this.pLogs.Controls.Add(this.splitter1);
+            this.pLogs.Controls.Add(this.btnRefresh);
+            this.pLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pLogs.Location = new System.Drawing.Point(3, 3);
+            this.pLogs.Name = "pLogs";
+            this.pLogs.Size = new System.Drawing.Size(655, 34);
+            this.pLogs.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.Color.White;
+            this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnRefresh.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnRefresh.Image = global::SmayDbEditor.UserInterface.Properties.Resources.refresh_24;
+            this.btnRefresh.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRefresh.Location = new System.Drawing.Point(0, 0);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(109, 34);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "Odśwież";
+            this.btnRefresh.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(109, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(206, 34);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(358, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numer dokumentu:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(500, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // LogsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 455);
+            this.Controls.Add(this.tlpLogs);
+            this.Name = "LogsForm";
+            this.ShowIcon = false;
+            this.Text = "Logi";
+            this.tlpLogs.ResumeLayout(false);
+            this.pLogs.ResumeLayout(false);
+            this.pLogs.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpLogs;
+        private System.Windows.Forms.Panel pLogs;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
