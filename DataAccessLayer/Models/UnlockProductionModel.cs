5cc0b31e03df0dec7ea95fdba639686aee6fcfde
Tomasz Banach <tom.banach98@gmail.com>
2021-12-19 16:14:29 +0000

    UnlockProductionModel / ViewModel OrderStatisticModel / ViewModel Imported from Changeset 146

---
+++ DataAccessLayer/Models/UnlockProductionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmayDbEditor.DataAccessLayer.Models
+{
+    public class UnlockProductionModel : EntityModel
+    {
+        public int ProcCode { get; set; }
+        public string ProcName { get; set; }
+        public string UserName { get; set; }
+        public DateTime TimeStarted { get; set; }
+    }
+}
---
+++ DataAccessLayer/ViewModel/UnlockProductionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SmayDbEditor.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmayDbEditor.DataAccessLayer.ViewModel
+{
+    public class UnlockProductionViewModel
+    {
+        public int proccode { get; set; }
+        public string procname { get; set; }
+        public string username { get; set; }
+        public DateTime timestarted { get; set; }
+    }
+}
